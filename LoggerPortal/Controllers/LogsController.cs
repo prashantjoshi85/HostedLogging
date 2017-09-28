c8831ebc80d61436421da4d03adf6c2905b82506
Chintan-Naik <chintan.naik@oneadvanced.com>
2017-09-28 22:21:09 +0530

    LoggerPortal

---
+++ LoggerPortal/Controllers/LogsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoggerPortal.Models;
+
+namespace LoggerPortal.Controllers
+{
+    public class LogsController : Controller
+    {
+        Logger_Entities _db;
+        public LogsController()
+        {
+            _db = new Logger_Entities();
+        }
+
+        public ActionResult Logs()
+        {
+            var Clients = from c in _db.Clients
+                          select new
+                          {
+                              ClientId = c.ClientId,
+                              ClientName = c.ClientName
+                          };
+
+            SelectList listClients = new SelectList(Clients, "ClientId", "ClientName");
+            ViewBag.Clients = listClients;
+
+            var Applications = from a in _db.Applications
+                               select new
+                               {
+                                   ApplicationId = a.ApplicationId,
+                                   ApplicationName = a.ApplicationName
+                               };
+
+            SelectList listApplicationName = new SelectList(Applications, "ApplicationId", "ApplicationName");
+            ViewBag.Applications = listApplicationName;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogDetail(string logId)
+        {
+            return PartialView("LogDetail", _db.LogPayloads.Where(p => p.LogId.ToString() == logId).FirstOrDefault());
+        }
+    }
+}
---
+++ LoggerPortal/Models/LoggerClass.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoggerPortal.Models
+{
+    public interface ILogsDetailRepository
+    {
+        IEnumerable<LogsDetail> GetData(out int totalRecords, string globalSearch, int? limitOffset, int? limitRowCount, string orderBy, bool desc);
+        IEnumerable<LogsDetail> GetData(out int totalRecords, int? limitOffset, int? limitRowCount, string orderBy, bool desc);
+        IEnumerable<LogsDetail> GetData(out int totalRecords, string filterClientId, string filterApplicationId, int? limitOffset, int? limitRowCount, string orderBy, bool desc);
+    }
+    public class LogsDetailRepository : ILogsDetailRepository
+    {
+        public IEnumerable<LogsDetail> GetData(out int totalRecords, string filterClientId, string filterApplicationId, int? limitOffset, int? limitRowCount, string orderBy, bool desc)
+        {
+            return GetData(out totalRecords, null, filterClientId, filterApplicationId, limitOffset, limitRowCount, orderBy, desc);
+        }
+
+        public IEnumerable<LogsDetail> GetData(out int totalRecords, string globalSearch, int? limitOffset, int? limitRowCount, string orderBy, bool desc)
+        {
+            return GetData(out totalRecords, globalSearch, null, null, limitOffset, limitRowCount, orderBy, desc);
+        }
+
+        public IEnumerable<LogsDetail> GetData(out int totalRecords, string globalSearch, string filterClientId, string filterApplicationId, int? limitOffset, int? limitRowCount, string orderBy, bool desc)
+        {
+            using (var db = new Logger_Entities())
+            {
+                var query = db.LogsDetails.AsQueryable();
+
+                if (!String.IsNullOrWhiteSpace(filterClientId))
+                {
+                    query = query.Where(p => p.ClientId.ToString() == filterClientId);
+                }
+                if (!String.IsNullOrWhiteSpace(filterApplicationId))
+                {
+                    query = query.Where(p => p.ApplicationId.ToString() == filterApplicationId);
+                }
+
+                if (!String.IsNullOrWhiteSpace(globalSearch))
+                {
+                    query = query.Where(p => (p.ClientId + " " + p.ApplicationId).Contains(globalSearch));
+                }
+
+                totalRecords = query.Count();
+
+                if (!String.IsNullOrWhiteSpace(orderBy))
+                {
+                    switch (orderBy)
+                    {
+                        case "ClientName":
+                            if (!desc)
+                                query = query.OrderBy(p => p.ClientName);
+                            else
+                                query = query.OrderByDescending(p => p.ClientName);
+                            break;
+                        case "ApplicationName":
+                            if (!desc)
+                                query = query.OrderBy(p => p.ApplicationName);
+                            else
+                                query = query.OrderByDescending(p => p.ApplicationName);
+                            break;
+                        case "LogTypeName":
+                            if (!desc)
+                                query = query.OrderBy(p => p.LogTypeName);
+                            else
+                                query = query.OrderByDescending(p => p.LogTypeName);
+                            break;
+                        case "LogDateTime":
+                            if (!desc)
+                                query = query.OrderBy(p => p.LogDateTime);
+                            else
+                                query = query.OrderByDescending(p => p.LogDateTime);
+                            break;
+                    }
+                }
+
+                if (limitOffset.HasValue)
+                {
+                    query = query.Skip(limitOffset.Value).Take(limitRowCount.Value);
+                }
+
+                return query.ToList();
+            }
+        }
+
+        public IEnumerable<LogsDetail> GetData(out int totalRecords, int? limitOffset, int? limitRowCount, string orderBy, bool desc)
+        {
+            return GetData(out totalRecords, null, null, null, limitOffset, limitRowCount, orderBy, desc);
+        }
+    }
+}
